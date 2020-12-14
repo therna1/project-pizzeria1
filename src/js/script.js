b6ddfaeff7031858d8285da3478fd53a2345b0a0
Oliwia Gabrych <marsmusic@op.pl>
2020-12-14 18:00:32 +0100

    Add accordion

---
+++ src/js/script.js
@@ -0,0 +1,122 @@
+/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+
+{
+  'use strict';
+
+  const select = {
+    templateOf: {
+      menuProduct: '#template-menu-product',
+    },
+    containerOf: {
+      menu: '#product-list',
+      cart: '#cart',
+    },
+    all: {
+      menuProducts: '#product-list > .product',
+      menuProductsActive: '#product-list > .product.active',
+      formInputs: 'input, select',
+    },
+    menuProduct: {
+      clickable: '.product__header',
+      form: '.product__order',
+      priceElem: '.product__total-price .price',
+      imageWrapper: '.product__images',
+      amountWidget: '.widget-amount',
+      cartButton: '[href="#add-to-cart"]',
+    },
+    widgets: {
+      amount: {
+        input: 'input[name="amount"]',
+        linkDecrease: 'a[href="#less"]',
+        linkIncrease: 'a[href="#more"]',
+      },
+    },
+  };
+
+  const classNames = {
+    menuProduct: {
+      wrapperActive: 'active',
+      imageVisible: 'active',
+    },
+  };
+
+  const settings = {
+    amountWidget: {
+      defaultValue: 1,
+      defaultMin: 1,
+      defaultMax: 9,
+    }
+  };
+
+  const templates = {
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+  };
+
+  class Product {
+    constructor(id, data) {
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+    };
+
+    renderInMenu() {
+      const thisProduct = this;
+      const HTML = templates.menuProduct(thisProduct.data);
+      thisProduct.element = utils.createDOMFromHTML(HTML);
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      menuContainer.appendChild(thisProduct.element);
+    };
+
+    initAccordion() {
+      const thisProduct = this;
+      const clickableTrigger = thisProduct.element.querySelector('.product__header');
+
+      clickableTrigger.addEventListener('click', function (event){
+        event.preventDefault();
+        let activeProduct = document.querySelector('.product.active');
+
+        if (activeProduct && activeProduct !== thisProduct.element){
+          activeProduct.classList.remove('active');
+        };
+
+        thisProduct.element.classList.toggle('active');
+      });
+
+    }
+  };
+
+
+  const app = {
+    initMenu: function () {
+      const thisApp = this;
+
+      for (let productData in thisApp.data.products) {
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function () {
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
+
+    init: function () {
+      const thisApp = this;
+      console.log('*** App starting ***');
+      console.log('thisApp:', thisApp);
+      console.log('classNames:', classNames);
+      console.log('settings:', settings);
+      console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
+    },
+  };
+
+  app.init();
+};
