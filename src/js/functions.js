f349a35001e17dfbc19245141f4d026cbd11b4c0
Oliwia Gabrych <marsmusic@op.pl>
2020-12-20 17:50:30 +0100

    updated

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,4 +1,4 @@
-/* global Handlebars */
+/* global Handlebars, dataSource */
 
 const utils = {}; // eslint-disable-line no-unused-vars
 
@@ -14,7 +14,7 @@ utils.createPropIfUndefined = function(obj, key, value = []){
   }
 };
 
-utils.serializeFormToObject = function(form){
+uutils.serializeFormToObject = function(form){
   let output = {};
   if (typeof form == 'object' && form.nodeName == 'FORM') {
     for (let field of form.elements) {
@@ -36,6 +36,19 @@ utils.serializeFormToObject = function(form){
   return output;
 };
 
+utils.convertDataSourceToDbJson = function(){
+  const productJson = [];
+  for(let key in dataSource.products){
+    productJson.push(Object.assign({id: key}, dataSource.products[key]));
+  }
+
+  console.log(JSON.stringify({product: productJson, order: []}, null, '  '));
+};
+
 Handlebars.registerHelper('ifEquals', function(arg1, arg2, options) {
   return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
+Handlebars.registerHelper('joinValues', function(input, options) {
+  return Object.values(input).join(options.fn(this));
 });
