be4b88c79f056bc87736cc9ccbf1ea478e80c990
Oliwia <marsmusic@op.pl>
2021-01-28 20:59:25 +0100

    Add pages (order & booking)

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,64 @@
 
-import {settings, select} from './settings.js';
+import {settings, classNames, select} from './settings.js';
 import Product from './components/Product.js'
 import Cart from './components/Cart.js';
 
 
+
   const app = {
+    initPages: function(){
+      const thisApp = this;
+
+      thisApp.pages = document.querySelector(select.containerOf.pages).children;  
+      thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+      const IdFromHash = window.location.hash.replace('#/', '');
+      let pageMatchingHash = thisApp.pages[0].id;
+
+      for(let page of thisApp.pages){
+        if(page.id == IdFromHash){
+          pageMatchingHash = page.id;
+          break;
+        };
+      };
+
+      thisApp.activatePage(pageMatchingHash);
+     
+
+     for(let link of thisApp.navLinks){
+       link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(id);
+
+        window.location.hash ='#/' + id
+       });
+     };
+    },
+
+    activatePage: function(pageId){
+      const thisApp = this;
+
+      for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+      };
+
+      for(let link of thisApp.navLinks){
+        link.classList.toggle(
+          classNames.nav.active, 
+          link.getAttribute('href') == '#' + pageId
+        );
+      };
+    },
+
     initMenu: function () {
       const thisApp = this;
 
       for (let productData in thisApp.data.products) {
         new Product(thisApp.data.products[productData].id, thisApp.data.products[productData]);
-      }
+      };
     },
 
     initData: function () {
@@ -33,6 +81,8 @@ import Cart from './components/Cart.js';
     init: function () {
       const thisApp = this;
       thisApp.initData();
+
+      thisApp.initPages()
       
     },
 
